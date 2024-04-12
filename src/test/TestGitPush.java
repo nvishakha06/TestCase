939cc018537e6425031a785b39dcde2271edce33
nvishakha06 <nvishakha06@gmail.com>
2024-04-12 23:58:35 +0530

    second commit

--- src/test/TestGitPush.java
+++ src/test/TestGitPush.java
@@ -4,7 +4,9 @@ public class TestGitPush {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+       System.out.println("This is my TestCase1");
+       System.out.println("This is my TestCase2");
+       System.out.println("This is my TestCase3");
 	}
 
 }
